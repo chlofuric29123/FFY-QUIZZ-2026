72a18214980039213bae90cdadde64e5f2bccacf
chlofuric29123 <chlo.furic29@gmail.com>
2026-08-26 14:54:10 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
-// À remplacer par la configuration de ton projet Firebase.
-// Tant que ces champs restent vides, le site fonctionne en MODE DÉMO sur un seul téléphone.
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAK8XX4B4jE2V1NglPkMpezmAK_BSQeL7I",
+  authDomain: "week-end-copain.firebaseapp.com",
+  databaseURL: "https://week-end-copain-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "week-end-copain",
+  storageBucket: "week-end-copain.firebasestorage.app",
+  messagingSenderId: "948915344339",
+  appId: "1:948915344339:web:f8443408a8c0414b791526"
 };
